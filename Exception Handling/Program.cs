53cbaf30feeba22ecffcd98cd8e5c9e39781f19c
LimESPS <72739674+LimESPS@users.noreply.github.com>
2021-03-06 16:38:28 +0000

    Edited IsValid class and added TriangleException class
    TriangleException class throws an Argument Exception if triangle is invalid

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringToDouble("12");
+            StringToDouble("hello");
+            StringToDouble("1.29e325");
+            Console.ReadKey();
+        }
+        static void StringToDouble(string s)
+        {
+            bool valid = true;
+            Double result;
+            try
+            {
+                result = Convert.ToDouble(s);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("(FormatException) Cannot convert to double, probably because your entry is not a number");
+                valid = false;
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("(OverflowException) Your entry is too large");
+                valid = false;
+            }
+            finally
+            {
+                if(valid)
+                    Console.WriteLine($"Result for {s} is valid");
+            }
+            throw new FormatException(e);
+        }
+    }
+}
---
+++ Triangle classes with exception handling.cs
@@ -0,0 +1,50 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry1.lib
+{
+    
+    public class Triangle
+        
+    {
+        int _sideA; //member variables
+        int _sideB;
+        int _sideC;
+        public Triangle(int a, int b, int c)
+        {
+            _sideA = a;
+            _sideB = b;
+            _sideC = c;
+        }
+        public Triangle(int e) : this(e, e, e)
+        {
+
+        }
+        public bool IsRightAngled()
+        {
+            return (_sideA * _sideA + _sideB * _sideB == _sideC * _sideC);
+        }
+        public int perimeter()
+        {
+            return _sideA + _sideB + _sideC;
+        }
+        public void TriangleException(int a)
+        {
+            throw new ArgumentException($"Triangle is invalid, {a} is larger than sum of the 2 other sides");
+        }
+        public void IsValid()
+        {
+            List<int> lengths = new List<int> { _sideA, _sideB, _sideC };
+            foreach(int item in lengths)
+            {
+                if (item > lengths.Sum() - item)
+                {
+                    TriangleException(item);
+                }
+            }
+        }
+    }
+}
